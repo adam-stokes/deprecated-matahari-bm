62030b7a4451341f42009791b341631bde02ee88
Darryl L. Pierce <dpierce@redhat.com>
2010-07-06 15:40:12 -0400

    Removed the platform-specific Platform class.
    Deleted the files linux_platform.{cpp,h} and platform.{cpp,h}. All of
    that functionality was moved into the host.cpp file into conditionally
    compiled sections.
    
    No Windows code was added, just the Linux code relocated.

--- src/host.cpp
+++ src/host.cpp
@@ -1,5 +1,5 @@
-/* host.cpp - Copyright (C) 2009 Red Hat, Inc.
- * Written by Arjun Roy <arroy@redhat.com>
+/* host.cpp - Copyright (C) 2010 Red Hat, Inc.
+ * Written by Darryl L. Pierce <dpierce@redhat.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,14 +18,25 @@
  */
 
 #include <config.h>
+#include <cstdlib>
+#include <fstream>
 #include "host.h"
-
-#include "platform.h"
+#include <libvirt/libvirt.h>
+#include <pcre.h>
 #include <set>
+#include <stdexcept>
+#include <string>
+#include <sys/sysinfo.h>
+#include <sys/utsname.h>
+
+// TODO remove this wrapper once rhbz#583747 is fixed
+extern "C" {
+#include <libudev.h>
+}
 
 using namespace std;
 
-set<HostListener*> _listeners;
+set<HostListener*>   _listeners;
 unsigned int         _heartbeat_sequence;
 
 void
@@ -53,7 +64,7 @@ host_update_event()
       iter++)
     {
       (*iter)->heartbeat((unsigned long)__time,
-                         _heartbeat_sequence);
+			 _heartbeat_sequence);
     }
 
   for(set<HostListener*>::iterator iter = _listeners.begin();
@@ -67,49 +78,230 @@ host_update_event()
 string
 host_get_uuid()
 {
-  return Platform::instance()->getUUID();
+  static string uuid;
+
+  if(uuid.empty())
+    {
+
+#ifdef __linux__
+      ifstream input("/var/lib/dbus/machine-id");
+
+      if(input.is_open())
+	{
+	  getline(input, uuid);
+	  input.close();
+	}
+#endif
+
+    }
+
+  return uuid;
 }
 
 string
 host_get_hostname()
 {
-  return Platform::instance()->getHostname();
+  static string hostname;
+
+  if(hostname.empty())
+    {
+
+#ifdef __linux__
+      struct utsname details;
+
+      if(!uname(&details))
+	{
+	  hostname = string(details.nodename);
+	}
+ #endif
+
+    }
+
+  return hostname;
 }
 
 string
 host_get_hypervisor()
 {
-  return Platform::instance()->getHypervisor();
+  static string hypervisor;
+
+  if(hypervisor.empty())
+    {
+
+#ifdef HAVE_LIBVIRT
+      virConnectPtr lvconn = virConnectOpenReadOnly(NULL);
+
+      if(lvconn)
+	{
+	  hypervisor = string(virConnectGetType(lvconn));
+	  virConnectClose(lvconn);
+	}
+#endif
+
+    }
+
+  return hypervisor;
 }
 
 string
 host_get_architecture()
 {
-  return Platform::instance()->getArchitecture();
+  static string architecture;
+
+  if(architecture.empty())
+    {
+
+#ifdef __linux__
+      struct utsname details;
+
+      if(!uname(&details))
+	{
+	  architecture = string(details.machine);
+	}
+#endif
+
+    }
+
+  return architecture;
 }
 
 unsigned int
 host_get_memory()
 {
-  return Platform::instance()->getMemory();
+  static unsigned int memory = 0;
+
+  if(!memory)
+    {
+
+#ifdef __linux__
+      struct sysinfo sysinf;
+      if(!sysinfo(&sysinf))
+	{
+	  memory = sysinf.totalram / 1024L;
+	}
+#endif
+
+    }
+
+  return memory;
 }
 
 string
 host_get_cpu_model()
 {
-  return Platform::instance()->getCPUModel();
+  static string model;
+
+  if(model.empty())
+    {
+
+#ifdef __linux__
+      ifstream* input = new ifstream("/proc/cpuinfo");
+
+      if(input->is_open())
+	{
+	  string regexstr = "(.*\\S)\\s*:\\s*(\\S.*)";
+	  int expected = 3;
+	  int found[expected * 3];
+	  const char* pcre_error;
+	  int pcre_error_offset;
+	  pcre* regex;
+	  bool done = false;
+	  bool started = false;
+
+	  regex = pcre_compile(regexstr.c_str(), 0, &pcre_error, &pcre_error_offset, NULL);
+	  if(!regex) { throw runtime_error("Unable to compile regular expression."); }
+
+	  while(!input->eof() && !done)
+	    {
+	      string line;
+
+	      getline(*input, line);
+	      int match = pcre_exec(regex, NULL, line.c_str(), line.length(),
+				    0, PCRE_NOTEMPTY,found, expected * 3);
+
+	      if(match == expected)
+		{
+		  string name = line.substr(found[2], found[3] - found[2]);
+		  string value = line.substr(found[4], found[5] - found[4]);
+
+		  /* If we're at a second processor and we've already started,
+		     then we're done.
+		  */
+		  if (name == "processor")
+		    {
+		      if (started)
+			{
+			  done = true;
+			}
+		      else
+			{
+			  started = true;
+			}
+		    }
+		  else
+		    {
+		      if(name == "model name") model = value;
+		    }
+		}
+	    }
+	  input->close();
+	  delete input;
+	}
+#endif
+
+    }
+
+  return model;
 }
 
 unsigned int
 host_get_number_of_cpu_cores()
 {
-  return Platform::instance()->getNumberOfCPUCores();
+  unsigned int cores = 0;
+
+  if(!cores)
+    {
+
+#ifdef __linux__
+      struct udev* udev = udev_new();
+      struct udev_enumerate* enumerator = udev_enumerate_new(udev);
+
+      udev_enumerate_add_match_property(enumerator, "DRIVER", "processor");
+      if(!udev_enumerate_scan_devices(enumerator))
+	{
+	  struct udev_list_entry* entries = udev_enumerate_get_list_entry(enumerator);
+	  struct udev_list_entry* entry;
+
+	  udev_list_entry_foreach(entry, entries)
+	    {
+	      cores++;
+	    }
+	}
+
+      udev_enumerate_unref(enumerator);
+      udev_unref(udev);
+#endif
+
+    }
+
+  return cores;
 }
 
 double
 host_get_load_average()
 {
-  return Platform::instance()->getLoadAverage();
+  double result = 0.0;
+
+#ifdef __linux__
+  double load_average;
+  ifstream input;
+
+  input.open("/proc/loadavg", ios::in);
+  input >> result;
+  input.close();
+#endif
+
+  return result;
 }
 
 void
