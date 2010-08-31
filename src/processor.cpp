5096ef2bf9f034ef5d54b71a8de3652457cfaf8e
Andrew Beekhof <andrew@beekhof.net>
2010-08-31 11:39:10 +0200

    Include definitions missing from the mingw32 version of winnt.h when cross-compiling

--- src/processor.cpp
+++ src/processor.cpp
@@ -17,12 +17,58 @@
  * also available at http://www.gnu.org/copyleft/gpl.html.
  */
 
-#ifndef WIN32
-#include "config.h"
-#endif
-
 #ifdef WIN32
-#include <windows.h>
+#  include <windows.h>
+#  include <winbase.h>
+#  ifndef MSVC
+
+// Items missing from winnt.h from mingw32
+//   http://msdn.microsoft.com/en-us/library/ms683194(VS.85).aspx
+
+typedef enum _LOGICAL_PROCESSOR_RELATIONSHIP {
+  RelationProcessorCore,
+  RelationNumaNode,
+  RelationCache,
+  RelationProcessorPackage,
+  RelationGroup,
+  RelationAll                = 0xffff 
+} LOGICAL_PROCESSOR_RELATIONSHIP;
+
+typedef enum _PROCESSOR_CACHE_TYPE {
+  CacheUnified,
+  CacheInstruction,
+  CacheData,
+  CacheTrace 
+} PROCESSOR_CACHE_TYPE;
+
+typedef struct _CACHE_DESCRIPTOR {
+  BYTE                 Level;
+  BYTE                 Associativity;
+  WORD                 LineSize;
+  DWORD                Size;
+  PROCESSOR_CACHE_TYPE Type;
+} CACHE_DESCRIPTOR, *PCACHE_DESCRIPTOR;
+
+typedef struct _SYSTEM_LOGICAL_PROCESSOR_INFORMATION {
+  ULONG_PTR                      ProcessorMask;
+  LOGICAL_PROCESSOR_RELATIONSHIP Relationship;
+  union {
+    struct {
+      BYTE Flags;
+    } ProcessorCore;
+    struct {
+      DWORD NodeNumber;
+    } NumaNode;
+    CACHE_DESCRIPTOR Cache;
+    ULONGLONG        Reserved[2];
+  } ;
+} SYSTEM_LOGICAL_PROCESSOR_INFORMATION, *PSYSTEM_LOGICAL_PROCESSOR_INFORMATION;
+
+#  endif
+typedef BOOL (WINAPI* LPFN_GLPI)(PSYSTEM_LOGICAL_PROCESSOR_INFORMATION, PDWORD);
+
+#else
+#  include "config.h"
 #endif
 
 #include <fstream>
@@ -119,7 +165,6 @@ cpu_get_details()
       cpuinfo.cpus /= cpuinfo.cores;
     }
 #elif defined WIN32
-  typedef BOOL (WINAPI* LPFN_GLPI)(PSYSTEM_LOGICAL_PROCESSOR_INFORMATION, PDWORD);
   LPFN_GLPI proc;
   DWORD ret_length;
   PSYSTEM_LOGICAL_PROCESSOR_INFORMATION buffer, ptr;
@@ -166,6 +211,8 @@ cpu_get_details()
 	    {
 	    case RelationProcessorCore:    cpuinfo.cores++; break;
 	    case RelationProcessorPackage: cpuinfo.cpus++;  break;
+		default:
+		    break;
 	    }
 	}
 
