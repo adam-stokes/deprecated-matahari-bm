3a2dae1cb68a846f4f1036a0b301c2aaa720bd8c
Adam Stokes <astokes@fedoraproject.org>
2011-04-11 10:53:16 -0400

    Add console request reponses, generic

--- src/console/matahari-console.cpp
+++ src/console/matahari-console.cpp
@@ -16,50 +16,77 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include "qpid/console/ConsoleListener.h"
-#include "qpid/console/SessionManager.h"
-#include "qpid/sys/Time.h"
-#include <signal.h>
+#ifndef WIN32
+#include "config.h"
+#endif
 
+#include "matahari/mh_agent.h"
+#include <qmf/Data.h>
+#include <qmf/ConsoleEvent.h>
+#include <qmf/ConsoleSession.h>
+#include "qmf/org/matahariproject/Console.h"
+#include <qpid/sys/Time.h>
+
+#include <sstream>
 using namespace std;
-using namespace qpid::console;
 
-/* 
- * listener class for responding to the events raised by our
- * matahari agents
- */
+extern "C" {
+#include <sigar.h>
+#include "matahari/host.h"
+}
 
-class Listener : public ConsoleListener {
+class ConsoleAgent : public MatahariAgent
+{
 public:
-  ~Listener() {}
-  void brokerConnected(const Broker& broker) {
-    cout << "broker connected: " << broker << endl;
-  }
-
-  void brokerDisconnected(const Broker& broker) {
-    cout << "broker disconnected: " << broker << endl;
-  }
-
-  void event(Event& event) {
-    cout << "event raised: " << event << endl;
-  }
-
+    int setup(qmf::AgentSession session);
+    gboolean invoke(qmf::AgentSession session, qmf::AgentEvent event, gpointer user_data);
 };
 
 int main(int argc, char** argv)
 {
-  Listener listener;
-  qpid::client::ConnectionSettings settings;
+    ConsoleAgent *agent = new ConsoleAgent();;
+    int rc = agent->init(argc, argv, "console");
+    if(rc == 0) {
+	agent->run();
+    }
+    return rc;
+}
 
-  SessionManager::Settings smSettings;
-  smSettings.rcvObjects = false;
-  smSettings.rcvHeartbeats = false;
-  SessionManager sm(&listener, smSettings);
-  cout << "Adding broker" << endl;
-  Broker* broker = sm.addBroker(settings);
-  while(true)
-    qpid::sys::sleep(1);
+int
+ConsoleAgent::setup(qmf::AgentSession session)
+{
+    _instance = qmf::Data(_package.data_Console);
+    _instance.setProperty("uuid", host_get_uuid());
+    _instance.setProperty("hostname", host_get_hostname());
+    _instance.setProperty("status", "Waiting for console event");
+    session.addData(_instance);
 
-  sm.delBroker(broker);
-  return 0;
+    qmf::ConsoleSession _console_session;
+    _console_session = qmf::ConsoleSession(_amqp_connection);
+    _console_session.open();
+    qmf::Agent agent;
+    while (true) {
+	if(_console_session.getAgentCount() > 0) {
+	    agent = _console_session.getAgent(0);
+	    qmf::ConsoleEvent result(agent.query("{class:Console, package:'org.matahariproject', where:[eq, hostname, [quote, supa.kooba]]}"));
+	    if(result.getType() == qmf::CONSOLE_QUERY_RESPONSE) {
+		_instance.setProperty("status", "Received console event");
+	    }
+	}
+	qpid::sys::sleep(1);
+    }
+
+    return 0;
+}
+
+gboolean
+ConsoleAgent::invoke(qmf::AgentSession session, qmf::AgentEvent event, gpointer user_data)
+{
+    if(event.getType() == qmf::AGENT_METHOD) {
+//	const std::string& methodName(event.getMethodName());
+	goto bail;
+    }
+    session.methodSuccess(event);
+bail:
+    return TRUE;
 }
