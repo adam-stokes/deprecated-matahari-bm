c379c96d79ddd2cf8cc35ab6a764f293010afa07
Adam Stokes <astokes@fedoraproject.org>
2011-05-03 12:35:40 -0400

    Tests
    Various beaker tests not only to help us when developing but also help QE sanitize our code quicker.
    
    Signed-off-by: Adam Stokes <astokes@fedoraproject.org>

---
+++ src/tests/ssl/mh_ssl_test.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# mh_ssl_test.sh - Copyright (c) 2011 Red Hat, Inc.
+# Written by Adam Stokes <astokes@fedoraproject.org>
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+# 
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+. /usr/share/beakerlib/beakerlib.sh
+
+TEST="/matahari/Sanity/mh_ssl_test"
+PACKAGE="matahari"
+
+CERT_DIR=test_cert_db
+CERT_PW_FILE=cert.password
+TEST_HOSTNAME=127.0.0.1
+TEST_CLIENT_CERT=agent
+SSL_PORT=5674
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlAssertRpm $PACKAGE
+        rlAssertRpm nss-tools
+        rlRun "TmpDir=\`mktemp -d\` " 0 "Creating tmp directory"
+        rlRun "pushd $TmpDir"
+        rlRun "mkdir $CERT_DIR"
+    rlPhaseEnd
+
+    rlPhaseStartTest
+        rlRun "echo password > $CERT_PW_FILE" 0 "Creating password file"
+        rlAssertExists "$CERT_PW_FILE"
+        rlRun "certutil -N -d $CERT_DIR -f $CERT_PW_FILE" 0 "Creating certificate database"
+        rlAssertExists "$CERT_DIR/cert8.db"
+        rlRun "certutil -S -d $CERT_DIR -n $TEST_HOSTNAME -s 'CN=$TEST_HOSTNAME' -t 'CT,,' -x -f $CERT_PW_FILE -z /usr/bin/certutil" 0 "Creating broker certificate"
+        rlRun "certutil -L -d $CERT_DIR -n $TEST_HOSTNAME"
+        rlRun "certutil -S -d $CERT_DIR -n $TEST_CLIENT_CERT -s 'CN=$TEST_CLIENT_CERT' -t 'CT,,' -x -f $CERT_PW_FILE -z /usr/bin/certutil" 0 "Creating agent certficiate"
+        rlRun "certutil -L -d $CERT_DIR -n $TEST_CLIENT_CERT"
+    rlPhaseEnd
+    
+    rlPhaseStartCleanup
+        rlRun "popd"
+        rlRun "rm -r $TmpDir" 0 "Removing tmp directory"
+    rlPhaseEnd
+rlJournalEnd
+rlJournalPrintText
+
+#export QPID_SSL_CERT_DB=${CERT_DIR}
+#export QPID_SSL_CERT_PASSWORD_FILE=${CERT_PW_FILE}
---
+++ src/tests/t_dnssrv.c
@@ -0,0 +1,48 @@
+/* t_dnssrv.c - Copyright (c) 2011 Red Hat, Inc.
+ * Written by Adam Stokes <astokes@fedoraproject.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <glib.h>
+#include "dnssrv_private.h"
+
+int main(int argc, char **argv)
+{
+    struct srv_reply **srvl = NULL, *srv = NULL;
+    int i;
+
+    if(srvl = srv_lookup("qpid-broker", "tcp", "qpid-broker:49000")) {
+        srv = *srvl;
+        for(i = 1; srvl[i]; i++) {
+            if(srvl[i]->prio < srv->prio)
+                srv = srvl[i];
+        }
+
+        fprintf("\tAccessible QPID Broker: %s\n", srv->name);
+        return 0;
+    }
+    return -1;
+}
+
+/*
+ * Local Variables:
+ * mode: c
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ * vim: et
+ */
+
