5942ea6560415629bcc986bba8419bba5f81291b
Adam Stokes <astokes@fedoraproject.org>
2011-05-18 11:40:14 -0400

    -Updated connection test
    - Checks for running broker and make sure its listening on $PORT
    - verify's default set of running agents and if running verify a
      connection has been established to the broker.

--- src/tests/connection/mh_simple_connect.sh
+++ src/tests/connection/mh_simple_connect.sh
@@ -21,15 +21,12 @@
 TEST="/matahari/Sanity/mh_simple_connect"
 PACKAGE="matahari"
 
-TEST_HOSTNAME=$1
-TEST_PORT=$2
-TEST_AGENT=$3
-
-if [ ! $# == 3 ]; then
-    TEST_HOSTNAME=127.0.0.1
-    TEST_PORT=49000
-    TEST_AGENT=matahari-hostd
-fi
+TEST_DEFAULT_AGENTS=( matahari-qmf-hostd \
+                         matahari-qmf-networkd \
+                         matahari-qmf-serviced \
+                         matahari-qmf-configd  )
+                             
+MATAHARI_BROKER_PORT=`grep -P 'MATAHARI_PORT=\d+' /etc/sysconfig/matahari-broker|cut -f2 -d=`
 
 rlJournalStart
     rlPhaseStartSetup
@@ -37,10 +34,19 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest
-        rlRun "$TEST_AGENT --broker $TEST_HOSTNAME --port $TEST_PORT"
+    rlRun "lsof -i :$MATAHARI_BROKER_PORT | grep -q ':$MATAHARI_BROKER_PORT.*LISTEN'" "0" "Verify broker is listening on $MATAHARI_BROKER_PORT."
+    for i in "${TEST_DEFAULT_AGENTS[@]}"
+    do
+        rlRun "ps -ef|grep -v grep|grep -q $i" "0" "Verifying $i is running."
+        TEST_PID=`ps -ef|grep $i|grep -v grep|cut -f4 -d' '`
+        if [ $TEST_PID ]; then
+            rlRun "lsof -p $TEST_PID |grep -q '$MATAHARI_BROKER_PORT.*ESTABLISHED'" "0" "Verify $i is connected to broker."
+        fi
+    done
     rlPhaseEnd
     
     rlPhaseStartCleanup
+        unset $TEST_PID
     rlPhaseEnd
 rlJournalEnd
 rlJournalPrintText
