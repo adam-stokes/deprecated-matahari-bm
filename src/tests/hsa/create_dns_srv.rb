638292ddf6b5532a7cb85fbf140b15ad52611a52
Adam Stokes <astokes@fedoraproject.org>
2011-06-15 22:11:57 -0400

    - HSA Tests
      - Start implementing HSA (High Service Availability tests
        which include both configuration of local dns server with
        defined SRV records and using our dnssrv library for validation

---
+++ src/tests/hsa/create_dns_srv.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/ruby
+#
+# create_dns_srv.rb - Creates necessary configuration+records for testing hsa
+#
+# Written by Adam Stokes <astokes@fedoraproject.org>
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+# 
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+require 'hsa_lib'
+
+if __FILE__ == $0
+  File.exist?("/etc/named.conf")
+  build = HSA::Writer.new
+  puts "Writing Configurations"
+  build.named_conf
+end
