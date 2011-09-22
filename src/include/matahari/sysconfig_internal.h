3c5473fdae4cdcd9081997f03ea14710b29b0c6b
Adam Stokes <astokes@fedoraproject.org>
2011-09-22 11:53:44 -0400

    Sanitize keys file
    Signed-off-by: Adam Stokes <astokes@fedoraproject.org>

--- src/include/matahari/sysconfig_internal.h
+++ src/include/matahari/sysconfig_internal.h
@@ -48,13 +48,13 @@ mh_sysconfig_keys_dir_set(const char *path);
  * Make sure we have some precautions on what filename and paths are
  * accepted.
  *
- * \param[in] path of key file
+ * \param[in] key file
  *
  * \note The return of this routine must be freed with free()
  *
  * \return sanitized path
  */
 const char *
-mh_sanitize_keys_file(const char *path);
+mh_sanitize_keys_file(const char *key);
 
 #endif /* __MH_SYSCONFIG_INTERNAL_H__ */
