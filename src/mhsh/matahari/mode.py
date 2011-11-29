50ea8db40eaac81ff06a005bda71f94b6cd8f563
Adam Stokes <astokes@fedoraproject.org>
2011-11-29 11:40:45 -0500

    mhsh: Add methods to command modes
    Signed-off-by: Adam Stokes <astokes@fedoraproject.org>

--- src/mhsh/matahari/mode.py
+++ src/mhsh/matahari/mode.py
@@ -5,20 +5,56 @@ Modes for adding specific routines
 from interpreter import mode, command
 
 class RootMode(mode.Mode):
-    @command.Command('select host', host)
+    @command.Command('select', 'host', ['HOST'])
     def select(self, kw_host, arg_host):
         """ Select host based on existing hostnames """
         pass
 
-    @command.Command('class', package, klass)
-    def package_class(self, kw_klass, *klass_args):
+    @command.Command('class', ('package', 'PACKAGE'), 'CLASS')
+    def package_class(self, kw_class, *class_args):
+        pass
+
+    @command.Command('clear', 'subscriptions')
+    def clear(self, kw_clear, kw_subscriptions):
         pass
 
 class FilteredMode(RootMode):
-    pass
+    @command.Command('clear', 'selection')
+    def clear_selection(self, kw_clear, kw_selection):
+        pass
+
+    @command.Command('show', 'selection')
+    def show_selection(self, kw_show, kw_selection):
+        pass
 
 class ClassMode(RootMode):
-    pass
+    @command.Command('clear', 'class')
+    def clear_class(self, kw_clear, kw_class):
+        pass
+
+    @command.Command('select', 'property', 'PROPERTY', 'VALUE')
+    def select_property(self, kw_select, kw_property, arg_property, arg_value):
+        pass
+
+    @command.Command('call', 'method', ['ARGS'], {})
+    def call_method(self, kw_call, kw_method, *args, **kwargs):
+        pass
+
+    @command.Command('get', 'PROPERTY')
+    def get_property(self, kw_get, arg_property):
+        pass
+
+    @command.Command('set', 'PROPERTY', 'VALUE')
+    def set_property(self, kw_set, arg_property, arg_value):
+        pass
+
+    @command.Command('get', 'STATISTIC')
+    def get_statistic(self, kw_get, arg_statistic):
+        pass
+
+    @command.Command('subscribe', 'EVENT')
+    def subscribe_event(self, kw_subscribe, arg_event):
+        pass
 
 class FilteredClassMode(FilteredMode, ClassMode):
     pass
